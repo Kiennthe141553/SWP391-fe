d0b0bb7b738324301924fd733c4e98a14ef1a167
KienNTHE141553 <kiennthe141553@fpt.edu.vn>
2022-09-11 12:10:36 +0700

    push register

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,51 +1,35 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = "https://itp-be-deploy.herokuapp.com";
+const API_URL = "http://localhost:10004";
 
 class UserService {
-  getPublicContent() {
-    return axios.get(API_URL + "/api/homepage", { headers: authHeader() });
-  }
-
-  getListProduct() {
+  getListUser() {
     return axios.get(API_URL + "/api/products", { headers: authHeader() });
   }
 
-  getListRole() {
-    return axios.get(API_URL + "/api/roles", { headers: authHeader() });
-  }
-
-  createProduct(payload) {
-    return axios.post(API_URL + "/api/products", payload, { headers: authHeader() });
-  }
-
   createUser(payload) {
-    return axios.post(API_URL + "/api/user", payload, { headers: authHeader() });
+    return axios.post(API_URL + "/api/user", payload, {
+      headers: authHeader(),
+    });
   }
 
-  editProduct(id,payload) {
-    return axios.put(API_URL + `/api/products/${id}`, payload, { headers: authHeader() });
+  editUser(id, payload) {
+    return axios.put(API_URL + `/api/products/${id}`, payload, {
+      headers: authHeader(),
+    });
   }
 
-  removeProduct(id) {
-    return axios.delete(API_URL + `/api/products/${id}`,  { headers: authHeader() });
+  deactiveUser(id) {
+    return axios.delete(API_URL + `/api/products/${id}`, {
+      headers: authHeader(),
+    });
   }
 
-  getInfoUser() {
-    return axios.get(API_URL + "/api/users", { headers: authHeader() });
-  }
-
-  getListCategory() {
-    return axios.get(API_URL + "/api/categories", { headers: authHeader() });
-  }
-
-  getDetailProduct(id) {
-    return axios.get(API_URL + `/api/products/${id}`, { headers: authHeader() });
-  }
-
-  changePass(newPass, oldPass) {
-    return axios.post(API_URL + "/api/user/changePass", {newPass, oldPass}, { headers: authHeader() });
+  getDetailUser(id) {
+    return axios.get(API_URL + `/api/products/${id}`, {
+      headers: authHeader(),
+    });
   }
 }
 
